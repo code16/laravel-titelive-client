5af97f0dc9aef797271cb07396d109b2a92c1c9d
philippe <philippe@code16.fr>
2023-08-24 14:43:43 +0200

    Bind classes

--- src/LaravelTiteliveClientServiceProvider.php
+++ src/LaravelTiteliveClientServiceProvider.php
@@ -2,24 +2,45 @@
 
 namespace Code16\LaravelTiteliveClient;
 
+use Code16\LaravelTiteliveClient\Api\Clients\BookCache;
+use Code16\LaravelTiteliveClient\Api\Clients\BookCacheMockForDev;
+use Code16\LaravelTiteliveClient\Api\Clients\BookDirectoryClient;
+use Code16\LaravelTiteliveClient\Api\Clients\BookDirectoryMockClientForDev;
+use Code16\LaravelTiteliveClient\Api\Clients\RandomBasedOnRefreshDateBookCache;
+use Code16\LaravelTiteliveClient\Api\Clients\TiteLive\TiteLiveClient;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Code16\LaravelTiteliveClient\Commands\LaravelTiteliveClientCommand;
 
 class LaravelTiteliveClientServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-titelive-client')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-titelive-client_table')
-            ->hasCommand(LaravelTiteliveClientCommand::class);
+            ->hasConfigFile();
+    }
+
+    public function packageRegistered()
+    {
+        $this->app->bind(BookDirectoryClient::class, function ($app) {
+            if(config('laravel-titelive-client.book_directory.mock', false)) {
+                return new BookDirectoryMockClientForDev();
+            }
+
+            return new TiteLiveClient(
+                config('services.titelive.endpoint'),
+                config('services.titelive.client_number'),
+                config('services.titelive.login'),
+                config('services.titelive.password'),
+            );
+        });
+
+        $this->app->bind(BookCache::class, function ($app) {
+            if(config('laravel-titelive-client.book_directory.mock', false)) {
+                return new BookCacheMockForDev();
+            }
+
+            return $app->get(RandomBasedOnRefreshDateBookCache::class);
+        });
     }
 }
