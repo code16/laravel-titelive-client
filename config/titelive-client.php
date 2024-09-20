522cd512e6aa72934eec5306dcb5f2976b34b065
philippe <philippe@code16.fr>
2024-09-20 11:47:03 +0200

    Fix config

--- config/titelive-client.php
+++ config/titelive-client.php
@@ -6,6 +6,10 @@ return [
         'use_cache' => env('TITELIVE_CLIENT_BOOK_DIRECTORY_USE_CACHE', false),
         'cache_duration' => env('TITELIVE_CLIENT_BOOK_DIRECTORY_CACHE_DURATION_IN_MINUTES', 60 * 24),
         'api' => [
+            'login' => env('TITELIVE_LOGIN'),
+            'client_number' => env('TITELIVE_CLIENT_NUM'),
+            'password' => env('TITELIVE_PWD'),
+            'endpoint' => env('TITELIVE_ENDPOINT', 'https://search.epagine.fr/ws.php'),
             'retry' => [
                 'times' => env('TITELIVE_CLIENT_BOOK_DIRECTORY_API_RETRY_TIMES', 5),
                 'sleep_milliseconds' => env('TITELIVE_CLIENT_BOOK_DIRECTORY_API_RETRY_SLEEP_MILLISECONDS', 2000),
